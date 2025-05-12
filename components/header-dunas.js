17f06c705a7215fd06de3f2b9ae55455499537a9
Luis Angel Hernandez <rlwev@hotmail.com>
2025-05-12 13:01:50 -0500

    correccion de errores

--- components/header-dunas.js
+++ components/header-dunas.js
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next'
 import Image from "next/image";
+import Link from 'next/link';
 
 export default function HeaderDunas() {
   const { t } = useTranslation(); // Inicializar el hook useTranslation
@@ -9,9 +10,9 @@ export default function HeaderDunas() {
   return (
     <header className="bg-dunas text-white py-6 sticky top-0 z-40 shadow-md max-md:px-6">
       <div className="container mx-auto flex justify-center items-center gap-16 max-md:gap-10">
-        <Image src="/img/dunas/logo-dunas.png" alt="Fromsky Realtors Cabo Logo" width={150} height={70}/>
+        <Link href={'/'}><Image src="/img/dunas/logo-dunas.png" alt="Fromsky Realtors Cabo Logo" width={150} height={70}/></Link>
         <div className='border-r border-black h-10'></div>
-        <Image src="/img/dunas/logo.png" alt="Dunas Logo" width={200} height={70}/>
+        <Link href={'/'}><Image src="/img/dunas/logo.png" alt="Dunas Logo" width={200} height={70}/></Link>
       </div>
     </header>
   );
